ad450b10c19717f80c147ac8e328f9f5da1bf775
TeslaElectr <ivanleb171@gmail.com>
2022-12-18 15:56:27 +0300

    New Branch homework_01

--- homework_01/main.py
+++ homework_01/main.py
@@ -11,7 +11,8 @@ def power_numbers(*numbers):
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
-    return [num ** 2 for num in numbers]
+    return list(map(lambda x: x ** 2, numbers))
+    # return [num ** 2 for num in numbers]
 
 
 # filter types
@@ -20,7 +21,21 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers(numbers_list, filter_type):
+def filter_odd(numbers_list):
+    if (numbers_list % 2) != 0:
+        return True
+    else:
+        return False
+
+
+def filter_even(numbers_list):
+    if (numbers_list % 2) == 0:
+        return True
+    else:
+        return False
+
+
+def filter_numbers(numbers_list: list, filter_type: str):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -31,7 +46,13 @@ def filter_numbers(numbers_list, filter_type):
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+
     if filter_type == ODD:
-        return [num for num in numbers_list if num % 2 != 0]
+        return list(filter(filter_odd, numbers_list))
     elif filter_type == EVEN:
-        return [num for num in numbers_list if num % 2 == 0]
+        return list(filter(filter_even, numbers_list))
+
+    # if filter_type == ODD:
+    #     return [num for num in numbers_list if num % 2 != 0]
+    # elif filter_type == EVEN:
+    #     return [num for num in numbers_list if num % 2 == 0]

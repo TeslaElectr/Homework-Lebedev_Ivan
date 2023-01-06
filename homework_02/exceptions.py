23353e1fd404be5cfff176ea166f8695eef99a81
TeslaElectr <ivanleb171@gmail.com>
2023-01-06 18:41:46 +0300

    First try test homework_02

--- homework_02/exceptions.py
+++ homework_02/exceptions.py
@@ -4,3 +4,22 @@
 - NotEnoughFuel
 - CargoOverload
 """
+
+
+class LowFuelError(Exception):
+    def __init__(self, number):
+        self.text = 'Не хватает топлива для запуска'
+        self.number = number
+
+
+class NotEnoughFuel(Exception):
+    def __init__(self, number):
+        self.text = 'Недостаточно топлива'
+        self.number = number
+
+
+class CargoOverload(Exception):
+    def __init__(self, number):
+        self.text = 'Перегрущка'
+        self.number = number
+

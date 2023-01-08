ebb4980d18db3f0ca369d9aeb5cefa536f712080
TeslaElectr <ivanleb171@gmail.com>
2023-01-09 00:03:55 +0300

    fix in __init__ in base.py

--- homework_02/base.py
+++ homework_02/base.py
@@ -4,8 +4,7 @@ from exceptions import LowFuelError, NotEnoughFuel
 
 class Vehicle(ABC):
 
-    def __init__(self, weight, fuel, fuel_consumption):
-
+    def __init__(self, weight=1000, fuel=50, fuel_consumption=6):
         self.weight = weight
         self.fuel = fuel
         self.fuel_consumption = fuel_consumption
@@ -26,9 +25,11 @@ class Vehicle(ABC):
 
 
 if __name__ == '__main__':
-    v = Vehicle()
+    v = Vehicle(weight=1000, fuel=50, fuel_consumption=4)
     print(v.started)
     v.start()
     print(v.started)
     v.move(12.4)
+    print(f'{v.fuel=}')
+    v.move(0.06)
     print(f'{v.fuel=}')

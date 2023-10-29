1286a3304a714f47762e6261d222ed691e6071d5
Ivan <ivanleb171@gmail.com>
2023-10-29 23:11:59 +0300

    add sleep(5) in test_app/ def run_image() and hw

--- homework_03/views.py
+++ homework_03/views.py
@@ -3,7 +3,11 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+@router.get('/')
+def helloworld():
+    return {'message': 'Hello World'}
 
 @router.get("/ping")
 def ping():
     return {"message": "pong"}
+    
